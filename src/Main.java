586af6a194e24f7f36dc44d4c9a34b1f429f527f
Gabriel <g4br13l@ciencias.unam.mx>
2024-02-27 12:15:50 -0600

    modified

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main{
     public static void main(String[] holi){
-	String[] obj = {"1","1","1","2","2","2"};	
-	String[] names = {"Gabriel","Gaby","Serg","Did","Woo","Wonder"};
+	String[] obj = {"1","2","3","4","5","6","7","8"};	
+	String[] names = {"Gabriel","Juanpi","Daniel","Antonio"};
 	
 	//primer parámetro (número de integrantes)
 	Roulette roulette = new Roulette(names.length,obj,names);
